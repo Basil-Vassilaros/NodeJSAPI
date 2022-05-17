dd9cdbd798b641b342e4d3cbfd77259be7ddb9cd
Basil Vassilaros <basilvassilaros.itca@outlook.com>
2022-05-17 13:30:39 +0200

    Server setup API - App folder

--- server.js
+++ server.js
@@ -7,3 +7,14 @@
 //Delete an Object
 //Delete all Objects
 //Test the API
+
+// Methods      URLS                Actions
+// GET          api/tutorials       get all tutorials
+// GET          api/tutorials/:id   get tutorial by id
+// POST         api/tutorials/      add new tutorials
+// PUT          api/tutorials/:id   update tutorial by id
+// DELETE       api/tutorials/:id   remove tutorial by id
+// DELETE       api/tutorials       deletes all tutorials
+
+// test the API using Postman
+var a = require('express');
