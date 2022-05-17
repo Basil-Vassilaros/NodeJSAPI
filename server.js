f5cba6dc2973a168b355220f8a4a2d2a6938971b
Basil Vassilaros <basilvassilaros.itca@outlook.com>
2022-05-17 12:57:37 +0200

    Added a server

---
+++ server.js
@@ -0,0 +1,9 @@
+//Define Routes
+//Create a Controller
+//Create Objects
+//Retrieve Objects
+//Retrieve a Single Object
+//Update an Object
+//Delete an Object
+//Delete all Objects
+//Test the API
